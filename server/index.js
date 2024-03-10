692f0496b290170ac00f64ed2e437bd2c2845b87
drewriker <62220518+drewriker@users.noreply.github.com>
2024-03-10 15:12:22 -0600

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const { getCompliment, getFortune, getGoals, updateGoals, deleteGoals } = require('./controller')
-const baseURL = "/api/"
+const { getCompliment, getFortune, postGoal } = require("./controller");
+const baseURL = "/api/";
 
 app.use(cors());
 app.use(express.json());
 
-
 app.get(`${baseURL}compliment`, getCompliment);
-app.get(`${baseURL}fortune`, getFortune)
-
+app.get(`${baseURL}fortune`, getFortune);
+app.post(`${baseURL}goals/`, postGoal);
 
 app.listen(4000, () => console.log("Server running on 4000"));
